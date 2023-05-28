79f9f03a5c1af02844e2a200bbb45a149fb75922
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-28 17:25:05 +0530

    [Address Book System day23 start]

---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello AddressBook System day23");
+        }
+    }
+}
