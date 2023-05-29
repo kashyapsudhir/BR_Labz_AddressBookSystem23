62b462ee8cf4969efcffd9df4a53dfa7884162a1
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-29 15:19:42 +0530

    [delete the contact]

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -8,10 +8,35 @@ namespace AddressBookSystem
         {
             Console.WriteLine("Hello AddressBook System day23");
 
-            AddressBook addressBook = new AddressBook();
-            addressBook.Contact();
-            //addressBook.NewContactAdd();
-            //addressBook.Edit();
+            while (true)
+            {
+                Console.WriteLine("*********************************************************");
+                Console.WriteLine("1. Add member to Contact list \n2.View Members in Contact List\n3.Edit members Contacts list\n4.Delete members Contacts list\n5.Exit");
+                Console.WriteLine("Enter an option:");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        AddressDetails.AddMember();
+                        break;
+                    case 2:
+                        AddressDetails.ViewContacts();
+                        break;
+                    case 3:
+                        AddressDetails.EditDetails();
+                        break;
+                    case 4:
+                        AddressDetails.DeleteDetails();
+                        break;
+                    case 5:
+                                                   // to exit from main method
+                        Console.WriteLine("Exit");
+                        return;
+
+                }
+            }
+
+
+
         }
     }
 }
